3c89cfef1fad9f8e617da2c232195c34cec317c1
Armando Nougues <armando.nougues@sectoralarm.eu>
2022-04-21 23:57:49 +0200

    finished layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import Formulario from "./Formulario";
 import Header from "./Header";
 import ListadoPacientes from "./ListadoPacientes";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [pacientes, setPacientes] = useState([]);
+
   return (
-    <>
+    <div className="container mx-auto mt-20">
       <Header />
-      <Formulario />
-      <ListadoPacientes />
-    </>
+      <div className="mt-12 md:flex">
+        <Formulario />
+        <ListadoPacientes />
+      </div>
+    </div>
   );
 }
 
