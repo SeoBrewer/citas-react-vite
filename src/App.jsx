32928b664f2e82e63291b1c0c0c4444296c5791a
Armando Nougues <armando.nougues@sectoralarm.eu>
2022-04-25 18:02:57 +0200

    FInishing

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,24 @@
 import { useState, useEffect } from "react";
-import Formulario from "./Formulario";
-import Header from "./Header";
-import ListadoPacientes from "./ListadoPacientes";
+import Formulario from "./components/Formulario";
+import Header from "./components/Header";
+import ListadoPacientes from "./components/ListadoPacientes";
 
 function App() {
   const [pacientes, setPacientes] = useState([]);
   const [paciente, setPaciente] = useState({});
 
   useEffect(() => {
-    console.log("Componente Listo");
-  });
+    const obtenerLS = () => {
+      const pacientesLS = JSON.parse(localStorage.getItem("pacientes")) ?? [];
+      setPacientes(pacientesLS);
+    };
+    obtenerLS();
+    console.log(1);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem("pacientes", JSON.stringify(pacientes));
+  }, [pacientes]);
 
   const eliminarPaciente = (id) => {
     const pacientesActualizados = pacientes.filter(
@@ -21,11 +30,13 @@ function App() {
   return (
     <div className="container mx-auto mt-20">
       <Header />
+
       <div className="mt-12 md:flex">
         <Formulario
           pacientes={pacientes}
           setPacientes={setPacientes}
           paciente={paciente}
+          setPaciente={setPaciente}
         />
         <ListadoPacientes
           pacientes={pacientes}
--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,7 +1,7 @@
 import { useState, useEffect } from "react";
 import Error from "./Error";
 
-const Formulario = ({ pacientes, setPacientes, paciente }) => {
+const Formulario = ({ pacientes, setPacientes, paciente, setPaciente }) => {
   const [nombre, setNombre] = useState("");
   const [propietario, setPropietario] = useState("");
   const [email, setEmail] = useState("");
@@ -21,47 +21,49 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
   }, [paciente]);
 
   const generarId = () => {
-    const random = Math.random().toString(36).substring(2);
+    const random = Math.random().toString(36).substr(2);
     const fecha = Date.now().toString(36);
     return random + fecha;
   };
 
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    // Validación del Formulario
     if ([nombre, propietario, email, fecha, sintomas].includes("")) {
-      console.log("Hay al menos un campo vacio");
+      console.log("Hay Al Menos un campo vacio");
 
       setError(true);
       return;
     }
+
     setError(false);
 
     // Objeto de Paciente
-
     const objetoPaciente = {
       nombre,
       propietario,
       email,
       fecha,
       sintomas,
-      id: generarId(),
     };
 
     if (paciente.id) {
-      //Editando
+      // Editando el Registro
       objetoPaciente.id = paciente.id;
-      console.table(objetoPaciente);
       const pacientesActualizados = pacientes.map((pacienteState) =>
         pacienteState.id === paciente.id ? objetoPaciente : pacienteState
       );
+
       setPacientes(pacientesActualizados);
+      setPaciente({});
     } else {
-      //Nuevo Registro
+      // Nuevo registro
       objetoPaciente.id = generarId();
       setPacientes([...pacientes, objetoPaciente]);
     }
 
-    // Reiniciar el formulario
+    // Reiniciar el form
     setNombre("");
     setPropietario("");
     setEmail("");
@@ -70,17 +72,23 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
   };
 
   return (
-    <div className="md:w-1/2 lg:w-2/5">
+    <div className="md:w-1/2 lg:w-2/5 mx-5">
       <h2 className="font-black text-3xl text-center">Seguimiento Pacientes</h2>
+
       <p className="text-lg mt-5 text-center mb-10">
-        Añade pacientes y{" "}
-        <span className="text-indigo-600 font-bold ">Administralos</span>{" "}
+        Añade Pacientes y {""}
+        <span className="text-indigo-600 font-bold ">Administralos</span>
       </p>
+
       <form
         onSubmit={handleSubmit}
-        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10 mx-5"
+        className="bg-white shadow-md rounded-lg py-10 px-5 mb-10"
       >
-        {error && <Error mensaje={"Todos los campos son obligatorios"} />}
+        {error && (
+          <Error>
+            <p>Todos los campos son obligatorios</p>
+          </Error>
+        )}
         <div className="mb-5">
           <label
             htmlFor="mascota"
@@ -91,14 +99,13 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
           <input
             id="mascota"
             type="text"
-            placeholder="Nombre Mascota"
+            placeholder="Nombre de la Mascota"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={nombre}
-            onChange={(e) => {
-              setNombre(e.target.value);
-            }}
+            onChange={(e) => setNombre(e.target.value)}
           />
         </div>
+
         <div className="mb-5">
           <label
             htmlFor="propietario"
@@ -112,11 +119,10 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
             placeholder="Nombre del Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={propietario}
-            onChange={(e) => {
-              setPropietario(e.target.value);
-            }}
+            onChange={(e) => setPropietario(e.target.value)}
           />
         </div>
+
         <div className="mb-5">
           <label
             htmlFor="email"
@@ -130,11 +136,10 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
             placeholder="Email Contacto Propietario"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={email}
-            onChange={(e) => {
-              setEmail(e.target.value);
-            }}
+            onChange={(e) => setEmail(e.target.value)}
           />
         </div>
+
         <div className="mb-5">
           <label
             htmlFor="alta"
@@ -147,32 +152,30 @@ const Formulario = ({ pacientes, setPacientes, paciente }) => {
             type="date"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
             value={fecha}
-            onChange={(e) => {
-              setFecha(e.target.value);
-            }}
+            onChange={(e) => setFecha(e.target.value)}
           />
         </div>
+
         <div className="mb-5">
           <label
             htmlFor="sintomas"
             className="block text-gray-700 uppercase font-bold"
           >
-            Sintomas
+            Síntomas
           </label>
           <textarea
             id="sintomas"
             className="border-2 w-full p-2 mt-2 placeholder-gray-400 rounded-md"
+            placeholder="Describe los Síntomas"
             value={sintomas}
-            onChange={(e) => {
-              setSintomas(e.target.value);
-            }}
-            placeholder="Describe los Sintomas"
+            onChange={(e) => setSintomas(e.target.value)}
           />
         </div>
+
         <input
           type="submit"
-          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-all"
-          value={paciente.id ? "Editar paciente" : "Agregar paciente"}
+          className="bg-indigo-600 w-full p-3 text-white uppercase font-bold hover:bg-indigo-700 cursor-pointer transition-colors"
+          value={paciente.id ? "Editar Paciente" : "Agregar Paciente"}
         />
       </form>
     </div>
